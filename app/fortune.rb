33beadbe0999fa02a0de6c7e8b46843719f105e8
inotom <wdf7322@yahoo.co.jp>
2014-03-01 22:14:57 +0900

    Modify instance variable access

--- app/fortune.rb
+++ app/fortune.rb
@@ -10,19 +10,19 @@ class Fortune
     suesho_kichi: "末小吉"
   }
 
-  attr_accessor :weight, :lots
+  attr_writer :weight
 
   def initialize(lots = nil)
     raise ArgumentError.new("Expected a argument of lots") if lots.nil?
-    self.lots = lots
-    self.weight = LUCKY.to_a.sample.first
+    @lots = lots
+    @weight = LUCKY.to_a.sample.first
   end
 
   def draw(lot_type = nil)
     if lot_type
       _draw lot_type
     else
-      LUCKY[self.weight]
+      LUCKY[@weight]
     end
   end
 
@@ -33,8 +33,8 @@ class Fortune
     end
 
     def lots_filter(lot_type)
-      self.lots[lot_type].select { |k,v|
-        v == self.weight || v == :master
+      @lots[lot_type].select { |k,v|
+        v == @weight || v == :master
       }
     end
 end
